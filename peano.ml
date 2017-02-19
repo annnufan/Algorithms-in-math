aa225d7dbed0d3f15d89542bebcd44568ebc5e5c
annnufan <annnufan@gmail.com>
2017-02-19 10:57:22 +0300

    Add first HW

--- peano.ml
+++ peano.ml
@@ -1,40 +1,32 @@
 print_string "Hello, user.";;
 
-type num = Z|S of num;;
+type peano = Z | S of peano;;
+
+let rec peano_of_int x = if (x == 0) then  Z else S (peano_of_int (x - 1));;
+
+let rec int_of_peano p = match p with
+	    Z -> 0
+		| S xx -> 1 + int_of_peano xx;;
 
 let inc x = S x;;
 
 let rec add x y = match y with 
-					Z -> x 
-					| S yy -> S(add x yy);;
-
+		Z -> x 
+		| S yy -> S(add x yy);;
 
 let rec sub x y = match (x, y) with
-					(Z, yy) -> Z
-					| (xx, Z) -> xx
-					| (S xx, S yy) -> sub xx yy;;
+		(Z, yy) -> Z
+		| (xx, Z) -> xx
+		| (S xx, S yy) -> sub xx yy;;
 
 let rec mul x y = match y with
-					Z -> Z
-					| S yy -> add (mul x yy) x;;
+		Z -> Z
+		| S yy -> add (mul x yy) x;;
 
-let rec deg x y = match y with
-					Z -> S Z
-					| S yy -> mul (deg x yy) x;;
+let rec power x y = match y with
+		Z -> S Z
+		| S yy -> mul (power x yy) x;;
 
-
-let y = S (S Z);;
-let x = deg y y;;
-
-let rec print_num x = match x with 
-						Z -> print_string "\n"; print_int 0
-						| S xx -> print_num xx; print_string "\'";;
-
-
-print_num (mul x y);;
-
-print_num (add x y);;
-
-print_num (inc x);;
-
-print_num (sub x y);;
+let rec print_peano x = match x with 
+		Z -> print_string "\n"; print_int 0
+		| S xx -> print_num xx; print_string "\'";;
