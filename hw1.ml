d74a596370c4397432390075f88f3aea56ab600b
annnufan <annnufan@gmail.com>
2017-11-22 18:14:55 +0300

    Correct hw1

--- hw1.ml
+++ hw1.ml
@@ -1,56 +1,68 @@
-type peano = Z | S of peano;;
-type lambda = Var of string | Abs of string * lambda | App of lambda * lambda;;
+type peano = Z 
+	| S of peano;;
+
+type lambda = Var of string 
+	| Abs of string * lambda 
+	| App of lambda * lambda;;
                      
-let rec peano_of_int x = if (x == 0) then  Z else S (peano_of_int (x - 1));;
+let rec peano_of_int x = match x with
+	0 -> Z 
+	| _ -> S (peano_of_int (x - 1));;
 
 let rec int_of_peano p = match p with
-		Z -> 0
-		| S x -> 1 + int_of_peano x;;
+	Z -> 0
+	| S x -> 1 + int_of_peano x;;
 
 let inc x = S x;;
 
 let rec add x y = match y with 
-		Z -> x 
-		| S yy -> S(add x yy);;
+	Z -> x 
+	| S yy -> S(add x yy);;
 
-let rec mul x y =  match y with
-		Z -> Z
-		| S yy -> add (mul x yy) x;;
+let rec mul x y = match y with
+	Z -> Z
+	| S yy -> add (mul x yy) x;;
 
 let rec sub x y = match (x, y) with
-		(Z, yy) -> Z
-		| (xx, Z) -> xx
-		| (S xx, S yy) -> sub xx yy;;
+	(Z, yy) -> Z
+	| (xx, Z) -> xx
+	| (S xx, S yy) -> sub xx yy;;
 
 let rec power x y = match y with
-		Z -> S Z
-		| S yy -> mul (power x yy) x;;
+	Z -> S Z
+	| S yy -> mul (power x yy) x;;
 
 let rec print_peano x = match x with 
-		Z -> print_string "\n"; print_int 0
-		| S xx -> print_peano xx; print_string "\'";;
-                     
-type 'a list = Nil | Cons of 'a*('a list);;
-
-let rec append x y = match x with
-		Nil -> y
-		| Cons(n, xx) -> Cons(n, append xx y);;
+	Z -> print_string "\n"; print_int 0
+	| S xx -> print_peano xx; print_string "\'";;
 
 let rec reverse x y = match x with
-		Nil -> y
-		| Cons(n, xx) -> reverse xx (Cons(n, y));;
+	[] -> y
+	| n::xx -> reverse xx (n::y);;
                      
-(*let rev x = reverse x Nil;;*)
+let rev x = reverse x [];;
 
 let rec size x = match x with
-		Nil -> 0
-		| Cons(y, xx) -> (size xx) + 1;;
+	[] -> 0
+	| y::xx -> (size xx) + 1;;
 
-(*let rec merge_sort x = match x with
-		Nil -> Nil
-		| Cons(n, xx) -> merge (merge_sort (take Cons(n, xx) 0 (size Cons(n, xx))/2)) (merge_sort (take Cons(n, xx) (size Cons(n, xx))/2 (size Cons(n, xx)))) [];; *)
-let merge_sort x = failwith "Not implemented";;
-let rev x = failwith "Not implemented";;
+let rec merge x y = match x with
+	[] -> y
+	| head::tail -> match y with
+		[] -> x
+		| head1::tail1 -> if head < head1 then
+				head::(merge tail y)
+			else
+				head1::(merge x tail);;
+
+let rec take x left right = match x with
+	[] -> (left, right)
+	| head::tail -> take tail right (head::left);; 
+
+let rec merge_sort x = match x with
+		[] -> []
+		| n::xx -> let (head, tail) = take x [] [] in
+			merge (merge_sort head) (merge_sort tail);;
 
 let rec string_of_lambda str = match str with 
 	Var x -> x
